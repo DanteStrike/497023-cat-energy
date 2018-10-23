abf144ac765968c317ea92dd2d7b474deec6fd3f
Gevork Gevorkyan <bmstu.gg@gmail.com>
2018-10-23 23:42:50 +0300

    CORRECT criterion B11

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,8 +3,10 @@
 (function () {
   const MAIN_MENU_BUTTON_MENU = "header__button--menu";
   const MAIN_MENU_BUTTON_JS = "header__button--nojs";
+  const HEADER_BORDER = "header__wrap--border-bottom";
 
   let mainMenuButton = document.querySelector(".header__button");
+  let headerWrap = document.querySelector(".header__wrap")
 
   let onMainMenuButtonClick = function (evt) {
     evt.preventDefault();
@@ -12,8 +14,10 @@
     let target = evt.target;
 
     target.classList.toggle(MAIN_MENU_BUTTON_MENU);
+    headerWrap.classList.toggle(HEADER_BORDER);
   }
 
   mainMenuButton.classList.remove(MAIN_MENU_BUTTON_JS);
+  headerWrap.classList.toggle(HEADER_BORDER);
   mainMenuButton.addEventListener("click", onMainMenuButtonClick);
 })();
